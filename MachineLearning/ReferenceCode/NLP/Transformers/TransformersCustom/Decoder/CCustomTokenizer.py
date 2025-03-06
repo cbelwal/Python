ce7bb93f37034dd369cdfb3143a928b151f43668
Chaitanya Belwal <cbelwal@gmail.com>
2025-03-06 07:52:33 -0600

    General Updates

--- MachineLearning/ReferenceCode/NLP/Transformers/TransformersCustom/Decoder/CCustomTokenizer.py
+++ MachineLearning/ReferenceCode/NLP/Transformers/TransformersCustom/Decoder/CCustomTokenizer.py
@@ -4,8 +4,12 @@ class CCustomTokenizer:
         self.fileName = fileName
         self.wordToTokenId = {}
         self.tokenIdToWord = {}
-        self.regularTokenStartIdx = 100 # Anything below this is reserved for special tokens
+        self.tokenIdxToTokenId = {}
+        # Do not set this to higher values as the vocab size in nn.embeddings layer
+        # is based on the index
+        self.regularTokenStartIdx = 5 # Anything below this is reserved for special tokens
         self.maxLen = 0
+        self.maxTokenId = 0
         self.allLines = []
         self.assignSpecialTokens()
         self.generateTokens()
@@ -16,13 +20,13 @@ class CCustomTokenizer:
     
     def assignSpecialTokens(self):
         self.startToken = 1
-        self.endToken = 2
+        self.sepToken = 2
         self.padToken = 0
         self.unkToken = 3
         self.wordToTokenId["CLS"] = self.startToken
         self.tokenIdToWord[self.startToken] = "CLS"
-        self.wordToTokenId["SEP"] = self.endToken
-        self.tokenIdToWord[self.endToken] = "SEP"
+        self.wordToTokenId["SEP"] = self.sepToken
+        self.tokenIdToWord[self.sepToken] = "SEP"
         self.wordToTokenId["PAD"] = self.padToken
         self.tokenIdToWord[self.padToken] = "PAD"
         self.wordToTokenId["UNK"] = self.unkToken
@@ -31,9 +35,12 @@ class CCustomTokenizer:
     def getPadTokenId(self):
         return self.padToken
 
+    def getSepTokenId(self):
+        return self.sepToken
+
     def generateTokens(self):
         allRawLines = self.read_file()
-        idx = self.regularTokenStartIdx + 1
+        tokenId = self.regularTokenStartIdx + 1
         for line in allRawLines:
             if line.strip() == "":
                 continue
@@ -50,17 +57,27 @@ class CCustomTokenizer:
             if(len(words) > self.maxLen):
                 self.maxLen = len(words)
 
+            idx = 0 # Mainly for use to get the logits index
             for word in words:
                 if word not in self.wordToTokenId:
-                    self.wordToTokenId[word] = idx
-                    self.tokenIdToWord[idx] = word
+                    self.wordToTokenId[word] = tokenId
+                    self.tokenIdToWord[tokenId] = word
+                    self.tokenIdxToTokenId[idx] = tokenId
                     idx += 1
+                    tokenId += 1
+            self.maxTokenId = tokenId
 
     def getMaxLen(self):
         return self.maxLen + 2 # For CLS and SEP tokens
 
+    def getMaxTokenId(self):
+        return self.maxTokenId
+
     def getVocabSize(self):
         return len(self.wordToTokenId)
+
+    def getTokenIdforIdx(self, idx):
+        return self.tokenIdxToTokenId[idx]
 
     def getWordForTokenId(self, tokenId):
         return self.tokenIdToWord[tokenId]
@@ -68,7 +85,7 @@ class CCustomTokenizer:
     def getTokenIdForWord(self, word):
         return self.wordToTokenId[word]
 
-    def getTokenizedSentence(self, sentence,maxLen=None):
+    def encodeTokenizedSentence(self, sentence,maxLen=None):
         if maxLen is None:
             maxLen = self.getMaxLen()
         words = sentence.split(' ')
@@ -96,7 +113,7 @@ class CCustomTokenizer:
             maxLen = self.getMaxLen()
         allRows = []
         for line in self.allLines:
-            tokens = self.getTokenizedSentence(line,maxLen)
+            tokens = self.encodeTokenizedSentence(line,maxLen)
             allRows.append(tokens)
         return allRows
 
@@ -106,7 +123,7 @@ if __name__=="__main__":
     print("Total tokens: ", tokenizer.getVocabSize())
     print("MaxLen: ", tokenizer.getMaxLen())
     sentence = "the cat is good"
-    tokens = tokenizer.getTokenizedSentence(sentence)
+    tokens = tokenizer.encodeTokenizedSentence(sentence)
     print("Tokens: ", tokens)
     decodedSentence = tokenizer.decodeTokenizedSentence(tokens)
     print("Decoded sentence: ", decodedSentence)
