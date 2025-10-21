9cf508069ebcbba5ea23df000bdd3f998cf311ab
Chaitanya Belwal <cbelwal@gmail.com>
2025-10-21 09:21:23 -0500

    Add core classes and methods for user embeddings and database management

--- MachineLearning/Research/UserEmbeddingsForMCP/Algorithm/CComputeEmbeddings.py
+++ MachineLearning/Research/UserEmbeddingsForMCP/Algorithm/CComputeEmbeddings.py
@@ -2,62 +2,49 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class SingleLinear(nn.Module):
-    def __init__(self, embeddingDimensions=4, totalNumberOfTools=2):
-        super().__init__()
-        # Need to set seed for reproducibility, has to be defined 
-        # by setting up layet as weights are assigned at that level
-        torch.manual_seed(1) 
-        # nn.Linear (input features, output features)
-        self.linear = nn.Linear(embeddingDimensions,
-                                totalNumberOfTools, bias=False)
+class CComputeEmbeddings():
+    def train(model, x, y, epochs=50, lr=0.01):
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+        criterion = nn.MSELoss()
+        for epoch in range(epochs):
+            # For each tool per user
+            optimizer.zero_grad()
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+            if epoch % 5 == 0:
+                print('Epoch:', epoch, 'Loss:', loss.item())
+        print('Epoch:', epoch, 'Loss:', loss.item())
 
-    def forward(self, x):
-        y = self.linear(x)
-        return y
+    # Function to compute cosine similarity between two vectors using numpy
+    def compute_cosine_similarity(tensor1, tensor2):
+        output = F.cosine_similarity(tensor1, tensor2, dim=0)
+        return output
 
-def train(model, x, y, epochs=50, lr=0.01):
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-    criterion = nn.MSELoss()
-    for epoch in range(epochs):
-        # For each tool per user
-        optimizer.zero_grad()
-        y_pred = model(x)
-        loss = criterion(y_pred, y)
-        loss.backward()
-        optimizer.step()
-        if epoch % 5 == 0:
-            print('Epoch:', epoch, 'Loss:', loss.item())
-    print('Epoch:', epoch, 'Loss:', loss.item())
-
-# Function to compute cosine similarity between two vectors using numpy
-def compute_cosine_similarity(tensor1, tensor2):
-    output = F.cosine_similarity(tensor1, tensor2, dim=0)
-    return output
-
-def get_test_MAT_tau_u(totalNumberOfUsers, totalNumberOfTools):
-    MAT_tau_u = torch.zeros(totalNumberOfUsers, totalNumberOfTools)
-    # ------ Manually Assign Values
-    MAT_tau_u[0][0] = 1 # user 0, tool 0
-    MAT_tau_u[0][1] = 2 # user 0, tool 1
-    MAT_tau_u[0][2] = 2 # user 0, tool 2
-    MAT_tau_u[0][3] = 0 # user 0, tool 3
-    MAT_tau_u[1][0] = 3 # user 1, tool 0
-    MAT_tau_u[1][1] = 1 # user 1, tool 1
-    MAT_tau_u[1][2] = 0 # user 1, tool 2
-    MAT_tau_u[1][3] = 2 # user 1, tool 3
-    # Canary #1: user 2 has same values as user 0
-    MAT_tau_u[2][0] = 1 # user 2, tool 0
-    MAT_tau_u[2][1] = 2 # user 2, tool 1
-    MAT_tau_u[2][2] = 2 # user 2, tool 2
-    MAT_tau_u[2][3] = 0 # user 2, tool 3
-    # Canary #2: user 3 has close values as user 1
-    MAT_tau_u[3][0] = 3 # user 1, tool 0
-    MAT_tau_u[3][1] = 1 # user 1, tool 1
-    MAT_tau_u[3][2] = 1 # user 1, tool 2 -> Only difference
-    MAT_tau_u[3][3] = 2 # user 1, tool 3
-    #------------------------
-    return MAT_tau_u
+    def get_test_MAT_tau_u(totalNumberOfUsers, totalNumberOfTools):
+        MAT_tau_u = torch.zeros(totalNumberOfUsers, totalNumberOfTools)
+        # ------ Manually Assign Values
+        MAT_tau_u[0][0] = 1 # user 0, tool 0
+        MAT_tau_u[0][1] = 2 # user 0, tool 1
+        MAT_tau_u[0][2] = 2 # user 0, tool 2
+        MAT_tau_u[0][3] = 0 # user 0, tool 3
+        MAT_tau_u[1][0] = 3 # user 1, tool 0
+        MAT_tau_u[1][1] = 1 # user 1, tool 1
+        MAT_tau_u[1][2] = 0 # user 1, tool 2
+        MAT_tau_u[1][3] = 2 # user 1, tool 3
+        # Canary #1: user 2 has same values as user 0
+        MAT_tau_u[2][0] = 1 # user 2, tool 0
+        MAT_tau_u[2][1] = 2 # user 2, tool 1
+        MAT_tau_u[2][2] = 2 # user 2, tool 2
+        MAT_tau_u[2][3] = 0 # user 2, tool 3
+        # Canary #2: user 3 has close values as user 1
+        MAT_tau_u[3][0] = 3 # user 1, tool 0
+        MAT_tau_u[3][1] = 1 # user 1, tool 1
+        MAT_tau_u[3][2] = 1 # user 1, tool 2 -> Only difference
+        MAT_tau_u[3][3] = 2 # user 1, tool 3
+        #------------------------
+        return MAT_tau_u
 
 if __name__== "__main__":
     # ---------- Data Generation
